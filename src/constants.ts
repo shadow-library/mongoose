d7de267a2a42e790a2f9688371c7dec0a77047e2
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2024-11-18 19:44:49 +0400

    feat: prop decorator (#1)

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+/**
+ * Importing npm packages
+ */
+
+/**
+ * Importing user defined packages
+ */
+
+/**
+ * Defining types
+ */
+
+/**
+ * Declaring the constants
+ */
+export const DESIGN_TYPE_METADATA_KEY = 'design:type';
+
+export const PROP_METADATA_KEY = Symbol('mongoose:prop');
+export const SCHEMA_METADATA_KEY = Symbol('mongoose:schema');
+export const SCHEMA_FIELDS_METADATA_KEY = Symbol('mongoose:schema:keys');
