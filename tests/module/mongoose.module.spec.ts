835759aeb217291b7ca2f9871ff305dde1ac4c40
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2025-06-18 21:34:00 +0400

    feat: mongoose module name using connection token (#16)

--- tests/module/mongoose.module.spec.ts
+++ tests/module/mongoose.module.spec.ts
@@ -18,15 +18,19 @@ import { MongooseModule } from '@shadow-library/mongoose';
  */
 
 describe('MongooseModule', () => {
-  const Module = MongooseModule.forRoot('mongodb://localhost:27017/test', { connectionName: 'TestConnection', appName: 'TestApp' });
+  const Module = MongooseModule.forRoot('mongodb://localhost:27017/test', { connectionName: 'Test', appName: 'TestApp' });
   const key = Reflect.getMetadataKeys(Module).find(key => key.toString() === 'Symbol(module-metadata)');
   const moduleMetadata = Reflect.getMetadata(key, Module);
+
+  it('should create the connection module with the correct class name', () => {
+    expect(Module.name).toBe('MongooseTestConnectionModule');
+  });
 
   it('should create providers for the connection', () => {
     expect(moduleMetadata).toStrictEqual({
       providers: [
         { token: MONGOOSE_MODULE_OPTIONS, useFactory: expect.any(Function) },
-        { token: 'TestConnectionConnection', useFactory: expect.any(Function), inject: [MONGOOSE_MODULE_OPTIONS] },
+        { token: 'TestConnection', useFactory: expect.any(Function), inject: [MONGOOSE_MODULE_OPTIONS] },
       ],
     });
   });
@@ -48,32 +52,50 @@ describe('MongooseModule', () => {
   });
 
   it('should return the cached class for the same connection name', () => {
-    const cachedClass = MongooseModule.forRoot('mongodb://localhost:27017/test', { connectionName: 'TestConnection' });
+    const cachedClass = MongooseModule.forRoot('mongodb://localhost:27017/test', { connectionName: 'Test' });
     expect(cachedClass).toBe(Module);
   });
 
   it('should return the correct mongoose options', () => {
     const options = moduleMetadata.providers[0].useFactory();
-    expect(options).toEqual({ uri: 'mongodb://localhost:27017/test', connectionName: 'TestConnection', appName: 'TestApp' });
+    expect(options).toEqual({ uri: 'mongodb://localhost:27017/test', connectionName: 'Test', appName: 'TestApp' });
   });
 
   it('should throw an error if the connection is not initialized', () => {
-    const Module = MongooseModule.forFeature([], 'NonExistentConnection');
+    const Module = MongooseModule.forFeature([], { connectionName: 'NonExistentConnection' });
     const key = Reflect.getMetadataKeys(Module).find(key => key.toString() === 'Symbol(module-metadata)');
     const moduleMetadata = Reflect.getMetadata(key, Module);
     const forwardRef = moduleMetadata.imports[0].forwardRef;
     expect(() => forwardRef()).toThrowError('MongooseModule is not initialized for connection: NonExistentConnection');
   });
 
+  it('should create the feature module with the Undefined if no model is provided', () => {
+    const modelDefinition: unknown[] = [];
+    const modelModule = MongooseModule.forFeature(modelDefinition as any, { connectionName: 'Test' });
+    expect(modelModule.name).toBe('MongooseUndefinedModelModule');
+  });
+
+  it('should create the feature module with the default class name', () => {
+    const modelDefinition = [{ name: 'Test', schema: {} }];
+    const modelModule = MongooseModule.forFeature(modelDefinition as any, { connectionName: 'Test' });
+    expect(modelModule.name).toBe('MongooseTestModelModule');
+  });
+
+  it('should create the feature module with the class name provided', () => {
+    const modelDefinition = [{ name: 'Test', schema: {} }];
+    const modelModule = MongooseModule.forFeature(modelDefinition as any, { connectionName: 'Test', featureName: 'TestModel' });
+    expect(modelModule.name).toBe('MongooseTestModelModule');
+  });
+
   it('should create providers for the models', () => {
     const modelDefinition = [{ name: 'Test', schema: {} }];
-    const modelModule = MongooseModule.forFeature(modelDefinition as any, 'TestConnection');
+    const modelModule = MongooseModule.forFeature(modelDefinition as any, { connectionName: 'Test' });
     const modelKey = Reflect.getMetadataKeys(modelModule).find(key => key.toString() === 'Symbol(module-metadata)');
     const modelModuleMetadata = Reflect.getMetadata(modelKey, modelModule);
     expect(modelModuleMetadata).toStrictEqual({
       imports: [expect.anything()],
-      providers: [{ token: 'TestConnectionConnection/TestModel', useFactory: expect.any(Function), inject: ['TestConnectionConnection'] }],
-      exports: ['TestConnectionConnection/TestModel'],
+      providers: [{ token: 'TestConnection/TestModel', useFactory: expect.any(Function), inject: ['TestConnection'] }],
+      exports: ['TestConnection/TestModel'],
     });
   });
 
@@ -85,16 +107,16 @@ describe('MongooseModule', () => {
         discriminators: [{ name: 'BaseOne', schema: {}, value: 'test' }],
       },
     ];
-    const modelModule = MongooseModule.forFeature(modelDefinition as any, 'TestConnection');
+    const modelModule = MongooseModule.forFeature(modelDefinition as any, { connectionName: 'Test' });
     const modelKey = Reflect.getMetadataKeys(modelModule).find(key => key.toString() === 'Symbol(module-metadata)');
     const modelModuleMetadata = Reflect.getMetadata(modelKey, modelModule);
     expect(modelModuleMetadata).toStrictEqual({
       imports: [expect.anything()],
       providers: [
-        { token: 'TestConnectionConnection/BaseModel', useFactory: expect.any(Function), inject: ['TestConnectionConnection'] },
-        { token: 'TestConnectionConnection/BaseOneModel', useFactory: expect.any(Function), inject: ['TestConnectionConnection/BaseModel'] },
+        { token: 'TestConnection/BaseModel', useFactory: expect.any(Function), inject: ['TestConnection'] },
+        { token: 'TestConnection/BaseOneModel', useFactory: expect.any(Function), inject: ['TestConnection/BaseModel'] },
       ],
-      exports: ['TestConnectionConnection/BaseModel', 'TestConnectionConnection/BaseOneModel'],
+      exports: ['TestConnection/BaseModel', 'TestConnection/BaseOneModel'],
     });
   });
 });
